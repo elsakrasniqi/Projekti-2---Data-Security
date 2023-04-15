d759de25aa74347a04e509a678fc13f4f1eb4146
emazech <130402584+emazech@users.noreply.github.com>
2023-04-15 18:30:26 +0200

    Update PolybiusSquareCipher.cpp

--- PolybiusSquareCipher.cpp
+++ PolybiusSquareCipher.cpp
@@ -7,3 +7,17 @@ char table[6][6] = {{'0','1','2','3','4','5'},
                     {'3','L','M','N','O','P'},
                     {'4','Q','R','S','T','U'},
                     {'5','V','W','X','Y','Z'}};
+
+void keys_table(){
+    string keys;
+    int a = 1, b = 1;
+    cout << "Enter keys for both rows and column: ";
+    getline(cin,keys);
+
+    for (int i = 0; i < keys.length(); i++){
+        table[0][b] = keys[i];
+        table[a][0] = keys[i];
+        b += 1;
+        a += 1;
+    }
+};
